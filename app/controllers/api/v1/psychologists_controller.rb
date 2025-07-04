ebef9d99170c0961cb377499994993af8163e1de
klauslube <klauslube93@gmail.com>
2025-07-04 01:01:31 +0000

    refact: identation

--- app/controllers/api/v1/psychologists_controller.rb
+++ app/controllers/api/v1/psychologists_controller.rb
@@ -1,13 +1,13 @@
 module Api
   module V1
-class PsychologistsController < ApplicationController
-  def available_appointments
-  psychologist = User.find(params[:id])
-  date = params[:date] || Date.today
+    class PsychologistsController < ApplicationController
+      def available_appointments
+        psychologist = User.find(params[:id])
+        date = params[:date] || Date.today
 
-  appointments = AvailableAppointmentsQuery.new(psychologist: psychologist, date: date).call
-  render json: appointments
-  end
-end
+        appointments = AvailableAppointmentsQuery.new(psychologist: psychologist, date: date).call
+        render json: appointments
+      end
+    end
   end
 end

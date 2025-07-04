4f791444c2093a5258e2627debca53a6a3b2d33b
klauslube <klauslube93@gmail.com>
2025-07-04 01:18:06 +0000

    feat: create caching to available appointments query

--- app/queries/available_appointments_query.rb
+++ app/queries/available_appointments_query.rb
@@ -16,22 +16,28 @@ class AvailableAppointmentsQuery
   attr_reader :psychologist, :date
 
   def available_appointments
-    occupied_hour_range = Appointment.where(
-      psychologist: psychologist,
-      status: :scheduled,
-      start_session: date.beginning_of_day..date.end_of_day
-    )
+    Rails.cache.fetch(cache_key, expires_in: 5.minutes) do
+      occupied_hour_range = Appointment.where(
+        psychologist: psychologist,
+        status: :scheduled,
+        start_session: date.beginning_of_day..date.end_of_day
+      ).pluck(:start_session, :end_session)
 
-    WORKING_HOURS.map do |hour|
-      start_time = Time.zone.local(date.year, date.month, date.day, hour)
+      WORKING_HOURS.map do |hour|
+        start_time = Time.zone.local(date.year, date.month, date.day, hour)
 
-      end_time = start_time + SESSION_DURATION
+        end_time = start_time + SESSION_DURATION
 
-      schedule_conflicted = occupied_hour_range.any? do |(start_booked, end_booked)|
-        start_time < end_booked && end_time > start_booked
-      end
+        schedule_conflicted = occupied_hour_range.any? do |(start_booked, end_booked)|
+          start_time < end_booked && end_time > start_booked
+        end
 
-      schedule_conflicted ? nil : start_time
-    end.compact
+        schedule_conflicted ? nil : start_time
+      end.compact
+    end
+  end
+
+  def cache_key
+    "available_appointments/#{psychologist.id}/#{date}"
   end
 end
